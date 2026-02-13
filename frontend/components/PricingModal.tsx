e24b7fbd81d70e94180297ad8a9bc06c01d17393
rongxanhcm <tranphule627@gmail.com>
2026-02-13 17:27:31 +0700

    paymentfix22

--- frontend/components/PricingModal.tsx
+++ frontend/components/PricingModal.tsx
@@ -8,11 +8,11 @@ import toast from "react-hot-toast";
 
 // --- CẤU HÌNH LINK THANH TOÁN ---
 // 1. Link Monthly (Đã lọc từ text bạn gửi)
-const CHECKOUT_URL_MONTHLY = "https://eloqua.lemonsqueezy.com/checkout/buy/76ea9484-fa92-43c9-ac90-https://eloqua.lemonsqueezy.com/checkout/buy/76ea9484-fa92-43c9-ac90-ef9c22e2beab?enabled=1304553";
+const CHECKOUT_URL_MONTHLY = "https://eloqua.lemonsqueezy.com/checkout/buy/76ea9484-fa92-43c9-ac90-ef9c22e2beab?enabled=1304553";
 
 // 2. Link Yearly (⚠️ BẠN CẦN THAY MÃ THẬT VÀO ĐÂY NẾU ĐÃ CÓ)
 // Hiện tại mình để tạm placeholder, nếu khách chọn Yearly sẽ lỗi 404
-const CHECKOUT_URL_YEARLY = "https://eloqua.lemonsqueezy.com/checkout/buy/https://eloqua.lemonsqueezy.com/checkout/buy/2b2a245a-5675-47b4-bd64-ccd3aedd8e01?enabled=1304546xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+const CHECKOUT_URL_YEARLY = "https://eloqua.lemonsqueezy.com/checkout/buy/2b2a245a-5675-47b4-bd64-ccd3aedd8e01?enabled=1304546";
 
 interface PricingModalProps {
   isOpen: boolean;
