7bb0e5e7f05f927d9654a0a4f4fb99ec551cc71b
rongxanhcm <tranphule627@gmail.com>
2026-02-20 02:47:21 +0700

    x

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: Request) {
   if (code) {
     // Thêm await vào dòng này
     const supabase = await createClient(); 
-    
+      
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`);
